d1d3e29b5ae8c5bd9c491e4c033e0849398035b7
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-04-05 11:42:04 +0700

    hienthicacloaihinh

--- src/hienthicacloaihinh.java
+++ src/hienthicacloaihinh.java
@@ -1,42 +1,62 @@
 import java.util.Scanner;
 public class hienthicacloaihinh {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Print the:");
-        int menu = scanner.nextInt();
-        if (menu == 1){
-            for (int i=0;i<3;i++){
-                for (int j=0;j<7;j++){
-                        System.out.printf("*");
-                }
-                System.out.println("\n");
-            }
-        }else if (menu == 2){
-            for (int i=0;i<5;i++){
-                for (int j=0;j<=i;j++){
-                        System.out.printf("*");
-                }
-                System.out.println("\n");
-            }
-        }else if (menu == 3){
-            for (int i=0;i<5;i++){
-                for (int j=5;j>i;j--){
-                    System.out.printf("*");
-                }
-                System.out.println("\n");
-            }
-        }else if (menu == 4   ){
-            for (int i=0;i<6;i=i+2){
-                for (int n=3;n>i;n=n-2){
-                    System.out.printf(" ");
-                }
-                for (int j=0;j<=i;j++){
-                    System.out.printf("*");
-                }
-                System.out.println("\n");
-            }
-        }
+        int choice=-1;
+        Scanner input = new Scanner(System.in);
+        while(choice!= 0) {
+            System.out.println("Menu");
+            System.out.println("1. Print the rectangle");
+            System.out.println("2. Print the square triangle botton-left");
+            System.out.println("3. Print the square triangle top-left");
+            System.out.println("4. Print isosceles triangle");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
 
+            switch (choice) {
+                case 1:
+                    for (int i=0;i<3;i++) {
+                        for (int j = 0; j < 7; j++) {
+                            System.out.printf("*");
+                        }
+                        System.out.println("\n");
+                    }
+                    break;
+                case 2:
+                    for (int i=0;i<5;i++) {
+                        for (int j = 0; j <= i; j++) {
+                            System.out.printf("*");
+                        }
+                        System.out.println("\n");
+                    }
+                    break;
+                case 3:
+                    for (int i=0;i<5;i++){
+                        for (int j=5;j>i;j--){
+                            System.out.printf("*");
+                        }
+                        System.out.println("\n");
+                    }
+                    break;
+                case 4:
+                    for (int i=0;i<6;i=i+2){
+                        for (int n=3;n>i;n=n-2){
+                            System.out.printf(" ");
+                        }
+                        for (int j=0;j<=i;j++){
+                            System.out.printf("*");
+                        }
+                        System.out.println("\n");
+                    }
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice!");
+            }
+
+
+        }
     }
 
 }
